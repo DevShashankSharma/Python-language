c5530701e1790dba88cb30e9debec3216ff1c4fd
DevShashankSharma <119869630+DevShashankSharma@users.noreply.github.com>
2023-11-23 15:56:09 +0530

    Typecasting in Python

---
+++ video08.py
@@ -0,0 +1,13 @@
+# Explicit TypeCasting
+a = "1"
+b = "2"
+print(a+b)
+print(int(a)+int(b))
+
+
+
+# Implicit TypeCasting
+c = 1.9
+d = 8   #int converted into float  (lower order  -> higher order)
+
+print(c+d)
