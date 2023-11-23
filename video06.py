b8ad9c58d8035e8eeb37113a490e6f10775c9a26
DevShashankSharma <119869630+DevShashankSharma@users.noreply.github.com>
2023-11-23 09:38:23 +0530

    Variables and Data Types

---
+++ video06.py
@@ -0,0 +1,35 @@
+a = 1
+# a1 = 9
+print(a)
+
+b = "Shashank"
+# print(b)
+
+c = True
+# print(c)
+
+d = None
+# print(d)
+
+e = 1.5
+# print(e)
+
+# print(a+a1)
+
+f = complex(1, 2)
+
+
+# print("The type of a is : ", type(a)) 
+# print("The type of b is : ", type(b))
+# print("The type of c is : ", type(c))
+# print("The type of d is : ", type(d))
+# print("The type of e is : ", type(e)) 
+# print("The type of f is : ", type(f))
+
+
+
+list1 = [1, 2, 3, 4, 5],["apple", "banana","orange"]
+print(list1)
+
+dict1 = {1:"apple", 2:"banana", 3:"orange"}
+print(dict1)
