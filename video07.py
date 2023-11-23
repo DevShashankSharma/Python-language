7147fcab825b0a48ee38cf0e0b59dda2fbc80740
DevShashankSharma <119869630+DevShashankSharma@users.noreply.github.com>
2023-11-23 13:19:01 +0530

    Exercise 1: Calculator using Python

---
+++ video07.py
@@ -0,0 +1,20 @@
+print(15+2)
+print(15-2)
+print(15*2)
+print(15/2)
+print(15//2)
+print(15%2)
+print(15**2)
+
+
+
+a = 56
+b = 23
+
+print("The value of ",a, " + ", b ," is ", a+b)
+print("The value of ",a, " - ", b ," is ", a-b)
+print("The value of ",a, " * ", b ," is ", a*b)
+print("The value of ",a, " / ", b ," is ", a/b)
+print("The value of ",a, " // ", b ," is ", a//b)
+print("The value of ",a, " % ", b ," is ", a%b)
+print("The value of ",a, " ** ", b ," is ", a**b)
