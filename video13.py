76aeb2cba90d57f68a23cf109db521480c59951a
DevShashankSharma <119869630+DevShashankSharma@users.noreply.github.com>
2023-11-24 08:19:26 +0530

    String Methods in Python

---
+++ video13.py
@@ -0,0 +1,52 @@
+a = "k!!Shashank!!!Shashank"  # string are immutable
+# print(len(a))
+# print(a.upper())
+# print(a.lower())
+
+# a = a.upper()
+# print(a)
+
+# print(a.rstrip("!"))
+# print(a.lstrip("!"))
+# print(a.replace("Shashank","Rohan"))
+
+# print(a.split("!"))
+
+
+blogHeadLog = "introduction tO js"
+# print(blogHeadLog.capitalize())
+
+str1 = "Welcome to the console!!!"
+# print(len(str1))
+# print(len(str1.center(50)))
+
+# print(a.count("Shashank"))
+# print(str1.endswith('!!!'))
+
+# print(str1.endswith('to',4,10))
+
+str2 = "He's name is Dan. He is an honest man."
+# print(str2.find("is"))
+# print(str2.find("ishh"))
+# print(str2.index("ishh"))
+
+
+str3 = "WelcomeToTheConsole"
+# print(str3.isalnum())
+# print(str3.isalpha())
+
+str4 = "Welcome To The Console"
+# print(str4.isspace())
+# print(str4.islower())
+
+str5 = "welcome to the console\n"
+# print(str5.isprintable())
+
+str6 = "                        " #space using tab or spacebar
+# print(str6.isspace())
+
+
+# print(str4.istitle())
+
+# print(str3.swapcase())
+# print(str4.title())
