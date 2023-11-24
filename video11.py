935767bef9c92069c978309c94e492cf18fc2dcb
DevShashankSharma <119869630+DevShashankSharma@users.noreply.github.com>
2023-11-24 06:54:07 +0530

    Strings in Python

---
+++ video11.py
@@ -0,0 +1,22 @@
+name = "Shashank"
+friend = "Rohan"
+anotherFriend = "Lovish"
+apple = 'he wants "apple" and "mango"'
+str = """Hello World,
+I am Shashank,
+I am learning Python"""
+
+# print("Hello "+name)
+# print("Hello "+friend)
+# print("Hello "+anotherFriend)
+# print("Hello "+apple)
+# print(str)
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+
+for character in str :
+    print(character)
+    
