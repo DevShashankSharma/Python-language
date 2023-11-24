5975436fe140b9458e209baa816aacdb9f423742
DevShashankSharma <119869630+DevShashankSharma@users.noreply.github.com>
2023-11-24 07:05:41 +0530

    Strings Slicing and Operations on Strings in Python

---
+++ video12.py
@@ -0,0 +1,16 @@
+names = "Shashank,Rohan,Lovish"
+# print(names[0:8])
+# print(len(names))
+
+# print(names[:4])
+# print(names[1:4])
+# print(names[:])
+
+# print(names[0:-3])    # converted into names[0:len(names)-3]
+# print(names[-1:-3])
+# print(names[-3:-1])
+
+
+#Quiz
+nm = "Shashank"
+print(nm[-4:-2])
