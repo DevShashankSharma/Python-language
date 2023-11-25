a8330746fdb05faf8c4afe15171c1068d1aebe70
DevShashankSharma <119869630+DevShashankSharma@users.noreply.github.com>
2023-11-25 13:35:58 +0530

    For Loops in Python

---
+++ video17.py
@@ -0,0 +1,22 @@
+# name = "Shashank"
+# for i in name:
+#     print(i)       
+#     if i == "h":
+#         print("This is something special") 
+        
+
+# colors = ["red", "green", "blue"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+
+# for k in range(10): 
+#     print(k)
+
+# for k in range(5, 10): 
+#     print(k)
+    
+for k in range(5, 10, 2): 
+    print(k)
