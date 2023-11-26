0e97b202eebe75d083898d628ff8fed05e5e282f
DevShashankSharma <119869630+DevShashankSharma@users.noreply.github.com>
2023-11-26 07:06:58 +0530

    break and continue in Python

---
+++ video19.py
@@ -0,0 +1,21 @@
+# for i in range(12):
+#     if(i ==10) :
+#         break
+#     print("5 X ",i+1," = ",5 * (i+1))
+
+    
+# for i in range(12):
+#     if(i ==10) :
+#         print("Skip the iteration")
+#         continue
+#     print("5 X ",i+1," = ",5 * (i+1))
+    
+
+
+# Do - While loop replica in python    
+i=0
+while True:
+    print(i)
+    i = i+1
+    if(i%100 == 0):
+        break
